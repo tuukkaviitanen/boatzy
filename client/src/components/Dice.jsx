35f7d0484b5628b7ad1b82020511328a35538b97
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-07-23 19:23:43 +0300

    Reworked Dice component

--- client/src/components/Dice.jsx
+++ client/src/components/Dice.jsx
@@ -1,4 +1,10 @@
 import { Button } from '@mui/material';
+import diceOne from '../assets/dice-one.svg';
+import diceTwo from '../assets/dice-two.svg';
+import diceThree from '../assets/dice-three.svg';
+import diceFour from '../assets/dice-four.svg';
+import diceFive from '../assets/dice-five.svg';
+import diceSix from '../assets/dice-six.svg';
 
 const styles = {
   image: {
@@ -9,10 +15,31 @@ const styles = {
   container: {},
 };
 
-const Dice = ({ imageSrc }) => {
+const selectIcon = (number) => {
+  switch (number) {
+    case 1:
+      return diceOne;
+    case 2:
+      return diceTwo;
+    case 3:
+      return diceThree;
+    case 4:
+      return diceFour;
+    case 5:
+      return diceFive;
+    case 6:
+      return diceSix;
+    default:
+      return null;
+  }
+};
+
+const Dice = ({ number }) => {
+  const image = selectIcon(number);
+
   return (
     <Button sx={styles.container}>
-      <img style={styles.image} src={imageSrc} />
+      <img style={styles.image} src={image} />
     </Button>
   );
 };
--- client/src/components/Dices.jsx
+++ client/src/components/Dices.jsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material';
 import Dice from './Dice';
-import diceOne from '../assets/dice-one.svg';
 
 const styles = {
   container: {
@@ -14,11 +13,11 @@ const styles = {
 const Dices = () => {
   return (
     <Box sx={styles.container}>
-      <Dice imageSrc={diceOne} />
-      <Dice imageSrc={diceOne} />
-      <Dice imageSrc={diceOne} />
-      <Dice imageSrc={diceOne} />
-      <Dice imageSrc={diceOne} />
+      <Dice number={1} />
+      <Dice number={2} />
+      <Dice number={3} />
+      <Dice number={4} />
+      <Dice number={5} />
     </Box>
   );
 };
