b22267baec39edbc6ae398220a888140b541c9ac
Tuukka Viitanen <97726090+tuukkaviitanen@users.noreply.github.com>
2024-07-27 00:54:39 +0300

    Added rules for each row (#32)
    - Each cell now generates a value using that row's rule
    
    Closes #11

---
+++ client/src/utils/diceHelpers.js
@@ -0,0 +1,51 @@
+import sum from './sum';
+
+export const findTwoPairs = (array) => {
+  const grouped = Object.groupBy(array, (number) => number);
+
+  const pairs = Object.entries(grouped).reduce(
+    (nOfAKinds, [key, group]) =>
+      group.length >= 2 && key > 0 ? [...nOfAKinds, key] : nOfAKinds,
+    [],
+  );
+
+  const topPairs = structuredClone(pairs).sort().slice(0, 2);
+
+  if (topPairs.length < 2) {
+    return 0;
+  }
+
+  const sumOfPairs = sum(topPairs.map((pair) => pair * 2));
+
+  return sumOfPairs;
+};
+
+export const findNOfAKind = (array, n) => {
+  const grouped = Object.groupBy(array, (number) => number);
+
+  const highest = Object.entries(grouped).reduce(
+    (highest, [key, group]) =>
+      group.length >= n && key > highest ? key : highest,
+    0,
+  );
+
+  const sum = highest * n;
+
+  return sum;
+};
+
+export const findFullHouse = (array) => {
+  const grouped = Object.groupBy(array, (number) => number);
+
+  const filteredGroups = Object.entries(grouped).filter(([key]) => key > 0);
+
+  const counts =
+    filteredGroups.length === 2 &&
+    filteredGroups.map(([, groups]) => groups.length);
+
+  const isFullHouse = counts && counts.includes(2) && counts.includes(3);
+
+  console.log({ counts, isFullHouse, filteredGroups, grouped });
+
+  return isFullHouse ? sum(array) : 0;
+};
---
+++ client/src/utils/sum.js
@@ -0,0 +1,7 @@
+/**
+ *
+ * @param {number[]} array
+ */
+const sum = (array) => array.reduce((sum, number) => sum + number, 0);
+
+export default sum;
