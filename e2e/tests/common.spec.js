5f5dd31d417d74b8f6f75e92f85f2d4de30c23eb
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-08-11 21:31:27 +0300

    Added some tests for board

--- e2e/tests/common.spec.js
+++ e2e/tests/common.spec.js
@@ -27,22 +27,52 @@ test.describe('UI', () => {
     await expect(buttons.nth(2)).toHaveText(/Reset game/i);
   });
 
-  test('should have 5 dices', async ({ page }) => {
-    const dicesContainer = page.getByTestId(/dices-container/);
-    const diceButtons = dicesContainer.getByRole('button');
-    await expect(diceButtons).toHaveCount(5);
+  test.describe('Board', () => {
+    test('should have correct amount of columns, rows and cells', async ({ page }) => {
+      const board = page.getByTestId(/board/)
+
+      const columns = board.locator("tr:first-child td")
+      const rows =  board.getByRole("row")
+
+      const cells = board.getByRole("cell")
+
+      await expect(columns).toHaveCount(3)
+      await expect(rows).toHaveCount(19)
+      await expect(cells).toHaveCount(57)
+    });
+
+    test("should show correct players", async ({page}) => {
+      const board = page.getByTestId(/board/)
+
+      const firstRow = board.getByRole("row").first()
+
+      const cells = firstRow.getByRole("cell")
+
+      await expect(cells.nth(1)).toHaveText(/Player 1/)
+      await expect(cells.nth(2)).toHaveText(/Player 2/)
+    })
   });
 
-  test('should have correct images rendered on dice buttons', async ({ page }) => {
-    const dicesContainer = page.getByTestId(/dices-container/);
+  test.describe('Dice buttons', () => {
+    test('should have 5 dices', async ({ page }) => {
+      const dicesContainer = page.getByTestId(/dices-container/);
+      const diceButtons = dicesContainer.getByRole('button');
+      await expect(diceButtons).toHaveCount(5);
+    });
 
-    for (const image of await dicesContainer.getByRole('img').all()) {
-      const isLoaded = await image.evaluate(
-      // @ts-ignore // Won't break anything if "complete" or "naturalHeight" properties don't exists, the tests just fail as they should
-        (img) => img.complete && img.naturalHeight !== 0
-      );
-      expect(isLoaded).toBeTruthy();
-      await expect(image).toHaveAttribute('src', '/src/assets/dice-zero.svg');
-    }
+    test('should have correct images rendered on dice buttons', async ({
+      page,
+    }) => {
+      const dicesContainer = page.getByTestId(/dices-container/);
+
+      for (const image of await dicesContainer.getByRole('img').all()) {
+        const isLoaded = await image.evaluate(
+          // @ts-ignore // Won't break anything if "complete" or "naturalHeight" properties don't exists, the tests just fail as they should
+          (img) => img.complete && img.naturalHeight !== 0
+        );
+        expect(isLoaded).toBeTruthy();
+        await expect(image).toHaveAttribute('src', '/src/assets/dice-zero.svg');
+      }
+    });
   });
 });
