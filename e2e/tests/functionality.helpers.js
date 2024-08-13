1f327f89d4f5ae9baedf3bddcb66f71a46b9c8d2
Tuukka Viitanen <tuukka.viitanen@gmail.com>
2024-08-13 20:20:12 +0300

    Added tests for user options

--- e2e/tests/functionality.helpers.js
+++ e2e/tests/functionality.helpers.js
@@ -1,59 +1,71 @@
-
-
+/**
+ * @param {import("playwright/test").Expect} expect
+ * @param {import("playwright-core").Locator} button
+ */
 export const checkDiceFunctionality = async (expect, button) => {
   const diceImage = button.getByRole('img');
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-zero.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-zero.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-one.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-one.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-two.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-two.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-three.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-three.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-four.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-four.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-five.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-five.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-six.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-six.svg');
 
   await button.click();
 
-  await expect(diceImage).toHaveAttribute(
-    'src',
-    '/src/assets/dice-zero.svg'
-  );
+  await expect(diceImage).toHaveAttribute('src', '/src/assets/dice-zero.svg');
+};
+
+/**
+ * @param {import("playwright-core").Page} page
+ * @returns {Promise<import("playwright-core").Locator>} Promise for cells locator
+ */
+export const getUserColumnCells = async (page) => {
+  const board = page.getByTestId(/board/);
+
+  const firstRow = board.getByRole('row').first();
+
+  const cells = firstRow.getByRole('cell');
+
+  return cells;
+};
+
+/**
+ * @param {import("playwright-core").Page} page
+ * @param {string} newName
+ */
+export const submitNewName = async (page, newName) => {
+  const userPopover = page.getByTestId('user-popover');
+
+  const renameField = userPopover.getByRole('textbox');
+
+  await renameField.clear();
+  await renameField.fill(newName);
+
+  userPopover.getByTestId('submit-button').click();
+};
+
+
+export const deleteUser = async (page) => {
+
 }
--- e2e/tests/functionality.spec.js
+++ e2e/tests/functionality.spec.js
@@ -1,5 +1,9 @@
 const { test, expect } = require('@playwright/test');
-const { checkDiceFunctionality } = require('./functionality.helpers');
+const {
+  checkDiceFunctionality,
+  getUserColumnCells,
+  submitNewName,
+} = require('./functionality.helpers');
 
 test.describe('Functionality', () => {
   test.beforeEach(async ({ page }) => {
@@ -14,6 +18,73 @@ test.describe('Functionality', () => {
       for (const diceButton of diceButtons) {
         await checkDiceFunctionality(expect, diceButton);
       }
+    });
+  });
+
+  test.describe('Menu buttons', () => {});
+
+  test.describe('Board', () => {
+    test.describe('User options', () => {
+      test('should open correctly when player clicked', async ({ page }) => {
+        const userColumnCells = await getUserColumnCells(page);
+
+        await userColumnCells.nth(1).click();
+
+        const userPopover = page.getByTestId('user-popover');
+
+        const renameField = userPopover.getByRole('textbox');
+
+        await expect(userPopover).toBeVisible();
+        await expect(renameField).toHaveValue('Player 1');
+        await expect(userPopover.getByTestId('submit-button')).toBeVisible();
+        await expect(userPopover.getByTestId('delete-button')).toBeVisible();
+      });
+
+      test('should successfully rename user', async ({ page }) => {
+        const userColumnCells = await getUserColumnCells(page);
+
+        await userColumnCells.nth(1).click();
+        await submitNewName(page, 'Test user 1');
+
+        await userColumnCells.nth(2).click();
+        await submitNewName(page, 'Test user 2');
+
+        expect(userColumnCells.nth(1)).toHaveText('Test user 1');
+        expect(userColumnCells.nth(2)).toHaveText('Test user 2');
+      });
+
+      test('should successfully delete user', async ({ page }) => {
+        const userColumnCells = await getUserColumnCells(page);
+
+        // Make sure
+        expect(userColumnCells).toHaveCount(3);
+        expect(userColumnCells.nth(1)).toHaveText("Player 1");
+
+        await userColumnCells.nth(1).click();
+
+        const userPopover = page.getByTestId('user-popover');
+
+        await userPopover.getByTestId('delete-button').click();
+
+        const updatedColumnCells = await getUserColumnCells(page);
+
+        expect(updatedColumnCells).toHaveCount(2);
+        expect(updatedColumnCells.nth(1)).toHaveText("Player 2");
+      });
+
+
+      test('should not delete last user', async ({ page }) => {
+        const userColumnCells = await getUserColumnCells(page);
+
+        const userPopover = page.getByTestId('user-popover');
+        const deleteButton = userPopover.getByTestId('delete-button')
+
+        await userColumnCells.nth(1).click();
+        await deleteButton.click();
+        await userColumnCells.nth(1).click();
+
+        await expect(deleteButton).toBeDisabled()
+      });
     });
   });
 });
